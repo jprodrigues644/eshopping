5dd7dccd31eacdffc6dbead5270e3b48e07913d5
jprodrigues644 <94868541+jprodrigues644@users.noreply.github.com>
2025-04-30 03:11:11 +0200

    commint init spring
    spring

---
+++ eshopping-backend/src/test/java/com/eshopping/eshopping_backend/EshoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshopping.eshopping_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
